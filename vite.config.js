bfd21fee1e8e2ca0fa0baccee40885839391596e
ruudvan03 <ordazruudvan@gmail.com>
2026-01-19 17:04:39 -0600

    Arreglo de detalles

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,20 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/sass/app.scss', // Asegúrate de que esta ruta sea la correcta
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // Esto silencia las advertencias de las dependencias (como Bootstrap)
+                quietDeps: true,
+                // Esto silencia advertencias específicas de las nuevas versiones de Sass
+                silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'mixed-decls', 'if-function'],
+            },
+        },
+    },
 });
